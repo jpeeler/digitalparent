09f264819ce043b9d38b7e9de3a57f5099e89546
pwightm <pwightm@31622b9c-f338-0410-9a15-2b66d3990ea2>
2007-10-05 16:58:18 +0000

    Part of previous commit.

--- DigitalParent/include/database.h
+++ DigitalParent/include/database.h
@@ -23,16 +23,16 @@ public:
 	Database();
 		 ~Database();
 
-	Profile getProfile(int diskID, int userID);
+	bool getProfile(Profile *profile);
 	bool storeProfile(Profile *profile);
 	bool removeProfile(Profile *profile);
 
-	User getUser(std::string& username, std::string& password);
+	bool getUser(User *user);
 	bool storeUser(User *user);
 	bool removeUser(User *user);
 
 	bool storeDisc(Disc *discInfo);
-	Disc getDisc(std::string& discName, int numChapters, long discLength);
+	bool getDisc(Disc *disc);
 
 private:
 	tntdb::Connection conn;
