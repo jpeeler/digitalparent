54b43b92659c57e9850c661caccc31c16f3c0c3d
ctemples <ctemples@31622b9c-f338-0410-9a15-2b66d3990ea2>
2007-10-21 21:57:41 +0000

    Plays a movie from a certain time

--- trunk/DP/src/media_player_dlg.cc
+++ trunk/DP/src/media_player_dlg.cc
@@ -7,13 +7,29 @@
 
 #include "config.h"
 #include "media_player_dlg.hh"
+#include "libvlc.h"
+
+libvlc_exception_t excp;
+libvlc_instance_t *inst;
+char *filename = "/home/ctemple/clemson/ece453/Marisa Tomei Hanes Commercial.gvi";
+char **test =NULL;
+const char *playtimes[2]={":start-time=5",":stop-time=8"};
+int item;
+
 
 void media_player_dlg::on_open_media_button_clicked()
 {  
+  libvlc_exception_init(&excp);
+  inst = libvlc_new (0, test, &excp);
+  item = libvlc_playlist_add_extended (inst, filename, NULL,2,playtimes, &excp);
 }
 
 void media_player_dlg::on_stop_button_clicked()
-{  
+{
+
+  libvlc_playlist_stop(inst,&excp);
+  usleep (10000000);
+  libvlc_destroy (inst);	
 }
 
 void media_player_dlg::on_previous_button_clicked()
@@ -30,9 +46,12 @@ void media_player_dlg::on_next_button_clicked()
 
 void media_player_dlg::on_pause_button_clicked()
 {  
+	libvlc_playlist_pause(inst,&excp);
+	//printf("Play time: " %d,libvlc_vlm_get_media_time(inst,var,&excp);
 }
 
 void media_player_dlg::on_play_button_clicked()
 {  
-system("/Projects/DP/src/demo");	
+//system("/Projects/DP/src/demo");
+	  libvlc_playlist_play (inst, item, 0, NULL, &excp);
 }
