3c75817ae4473a67829ae74b26ed4cdce98668fb
ctemples <ctemples@31622b9c-f338-0410-9a15-2b66d3990ea2>
2007-10-22 15:50:39 +0000

    added full functions

--- trunk/DP/src/media_player_dlg.cc
+++ trunk/DP/src/media_player_dlg.cc
@@ -9,19 +9,26 @@
 #include "media_player_dlg.hh"
 #include "libvlc.h"
 
+
 libvlc_exception_t excp;
 libvlc_instance_t *inst;
 char *filename = "/home/ctemple/clemson/ece453/Marisa Tomei Hanes Commercial.gvi";
 char **test =NULL;
-const char *playtimes[2]={":start-time=5",":stop-time=8"};
+const char *playtimes[3]={":start-time=1",":stop-time=5"};
+const char *playtimes2[2]={":start-time=11",":stop-time=15"};
 int item;
+int id;
+
 
 
 void media_player_dlg::on_open_media_button_clicked()
 {  
-  libvlc_exception_init(&excp);
-  inst = libvlc_new (0, test, &excp);
-  item = libvlc_playlist_add_extended (inst, filename, NULL,2,playtimes, &excp);
+	libvlc_exception_init(&excp);
+  	inst = libvlc_new (0, test, &excp);
+  	item = libvlc_playlist_add_extended (inst, filename, NULL,2,playtimes, &excp);
+	libvlc_playlist_add_extended (inst, filename, NULL,2,playtimes2, &excp);
+	id = libvlc_get_vlc_id(inst);
+	
 }
 
 void media_player_dlg::on_stop_button_clicked()
@@ -34,20 +41,25 @@ void media_player_dlg::on_stop_button_clicked()
 
 void media_player_dlg::on_previous_button_clicked()
 {  
+	libvlc_playlist_prev(inst,&excp);
 }
 
 void media_player_dlg::on_cut_button_clicked()
 {  
+	int time;
+	time = VLC_TimeGet(id);
+	printf("Play time:  %d\n",time);
 }
 
 void media_player_dlg::on_next_button_clicked()
 {  
+	//	VLC_SpeedFaster(id);
+	libvlc_playlist_next(inst,&excp);
 }
 
 void media_player_dlg::on_pause_button_clicked()
 {  
 	libvlc_playlist_pause(inst,&excp);
-	//printf("Play time: " %d,libvlc_vlm_get_media_time(inst,var,&excp);
 }
 
 void media_player_dlg::on_play_button_clicked()
