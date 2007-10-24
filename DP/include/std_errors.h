e72906f4fa2a8724378037b2a862da1a8cdfe447
pwightm <pwightm@31622b9c-f338-0410-9a15-2b66d3990ea2>
2007-10-24 20:17:18 +0000

    Added Error Code support.

---
+++ DP/include/std_errors.h
@@ -0,0 +1,26 @@
+/***************************************************************************
+ *            std_errors.h
+ *
+ *  Mon Oct 22 18:51:02 2007
+ ****************************************************************************/
+
+#ifndef _STD_ERRORS_H
+#define _STD_ERRORS_H
+
+#define SUCCESS 0
+#define GEN_ERROR 1
+
+/**
+ * Database Errors
+**/
+#define DB_GEN_ERROR 100
+#define DB_DATABASE_NOT_FOUND 101
+#define DB_UNKNOWN_USER 110
+#define DB_UNKNOWN_DISC 111
+#define DB_UNKNOWN_PROFILE 112
+#define DB_BAD_PASSWORD 120
+#define DB_USERNAME_IN_USE 121
+
+
+
+#endif /* _STD_ERRORS_H */
