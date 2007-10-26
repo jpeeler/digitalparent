bede0ab238d0214a4e050fd29579280f95fea033
pwightm <pwightm@31622b9c-f338-0410-9a15-2b66d3990ea2>
2007-10-26 03:22:35 +0000

    Added functions to return user and icon lists.  Also changed error codes from #defines to enum.

--- trunk/DP/include/std_errors.h
+++ trunk/DP/include/std_errors.h
@@ -7,12 +7,14 @@
 #ifndef _STD_ERRORS_H
 #define _STD_ERRORS_H
 
-#define SUCCESS 0
-#define GEN_ERROR 1
+//#define SUCCESS 0
+//#define GEN_ERROR 1
 
 /**
  * Database Errors
 **/
+
+/*
 #define DB_GEN_ERROR 100
 #define DB_DATABASE_NOT_FOUND 101
 #define DB_UNKNOWN_USER 110
@@ -20,7 +22,18 @@
 #define DB_UNKNOWN_PROFILE 112
 #define DB_BAD_PASSWORD 120
 #define DB_USERNAME_IN_USE 121
-
+*/
+typedef enum {
+	SUCCESS,
+	GEN_ERROR,
+	DB_GEN_ERROR,
+	DB_DATABASE_NOT_FOUND,
+	DB_UNKNOWN_USER,
+	DB_UNKNOWN_DISC,
+	DB_UNKNOWN_PROFILE,
+	DB_BAD_PASSWORD,
+	DB_USERNAME_IN_USE
+} ERRORS;
 
 
 #endif /* _STD_ERRORS_H */
