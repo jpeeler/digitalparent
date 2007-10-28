73612ae3bbc260afdd4a0e1c85eb89dbe1e173fc
jeremy.morey <jeremy.morey@31622b9c-f338-0410-9a15-2b66d3990ea2>
2007-10-28 22:59:01 +0000

    added login.hh files back in....im confused

--- DP/include/login_dlg.hh
+++ DP/include/login_dlg.hh
@@ -1,4 +1,4 @@
-// generated 2007/10/15 11:40:56 EDT by jmorey@jmorey-desktop.(none)
+// generated 2007/10/28 18:51:11 EDT by jmorey@jmorey-laptop.(none)
 // using glademm V2.6.0
 //
 // newer (non customized) versions of this file go to login_dlg.hh_new
@@ -17,8 +17,10 @@ class login_dlg : public login_dlg_glade
         
         void on_admin_psswrd_edit_box_editing_done();
         void on_previous_user_button_clicked();
-        void on_user_icon_select_button_clicked();
-        void on_next_user_button_clicked();
         void on_admin_login_button_clicked();
+        void on_next_user_button_clicked();
+        void on_user_icon_select_button_2_clicked();
+        void on_user_icon_select_button_1_clicked();
+        void on_user_icon_select_button_3_clicked();
 };
 #endif
