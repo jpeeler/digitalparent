5a932f76e1eb5290ed6d254ac26c9086750ebc24
jeremy.morey <jeremy.morey@31622b9c-f338-0410-9a15-2b66d3990ea2>
2007-10-28 16:32:43 +0000

    added clipbord reference

--- trunk/DP/include/dp_gui.h
+++ trunk/DP/include/dp_gui.h
@@ -16,10 +16,17 @@ typedef enum {
 	USER_PLAY,
 	ADMIN_PLAY
 } MODE;
-	
 User *m_current_user;
 std::vector<User> *m_user_list;
 Controller *m_control;
 MODE m_mode;
+Glib::RefPtr<Gtk::Clipboard> m_DPClipboard;
+
+class DP_Gui
+{
+	
+public:
+	void on_welcome_text_received(const Glib::ustring& text);
+};
 
 #endif //__DP_GUI__
